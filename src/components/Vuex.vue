aac6a446f8ff60643f170bf8e1099977a889fb0c
zrt <15970334351@163.com>
2024-04-18 20:55:56 +0800

    feat: vuex持久化

---
+++ src/components/Vuex.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+// 需要处理vuex中的数据，就需要从vuex中按需引入useStore
+import { useStore } from 'vuex'
+const store = useStore()
+
+const handleClick = () => {
+  // 同步修改
+  store.commit('updateUname', 'Kobe')
+
+  // 异步修改
+  store.dispatch('asyncUpdate', 40)
+}
+</script>
+
+<template>
+  <div>同步修改state：{{ $store.state.info.uname }}</div>
+  <span>----------------------------</span>
+  <div>异步修改state:{{ $store.state.info.age }}</div>
+  <span>----------------------------</span>
+  <div>getter数据：{{ $store.getters['format'] }}</div>
+  <button type="button" @click="handleClick">按钮</button>
+</template>
+
+<style scoped></style>
---
+++ src/store/index.ts
@@ -0,0 +1,40 @@
+import { createStore } from 'vuex'
+import createPersistedstate from 'vuex-persistedstate'
+
+export default createStore({
+  state: {
+    info: {
+      uname: 'Leo',
+      age: 21,
+    },
+  },
+  mutations: {
+    updateUname(state, val) {
+      state.info.uname = val
+    },
+    updateUage(state, val) {
+      state.info.age = val
+    },
+  },
+  actions: {
+    asyncUpdate(store, val) {
+      setTimeout(() => {
+        store.commit('updateUage', val)
+      }, 1000)
+    },
+  },
+  getters: {
+    format(state) {
+      return state.info.uname + '， meet you'
+    },
+  },
+  modules: {},
+  // 配置插件
+  plugins: [
+    // 持久化
+    createPersistedstate({
+      key: 'saveInfo',
+      paths: ['info'],
+    }),
+  ],
+})
